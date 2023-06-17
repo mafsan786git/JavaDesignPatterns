6475d351a2d038ceb16682c725a74e06e6555384
Mohd. Afsan Ahmad <mafsan78692@gmail.com>
2023-06-17 14:31:44 +0530

    added java code

--- DesignPatterns/src/com/codewithafsan/designpattern/TaxCalculator2020.java
+++ DesignPatterns/src/com/codewithafsan/designpattern/TaxCalculator2020.java
@@ -1,8 +1,9 @@
 package com.codewithafsan.designpattern;
 
 public class TaxCalculator2020 implements TaxCalculator {
+
     @Override
     public float calculateTax() {
-        return 2020;
+        return 0;
     }
 }
